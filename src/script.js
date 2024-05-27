47e89bbe81d025beeff2dd88e23f608368a3fa08
Lawal12maryam <156074402+Lawal12maryam@users.noreply.github.com>
2024-05-27 14:00:50 +0100

    another change

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function openNav() {
+  document.getElementById("sideBar").style.width = "100%";
+  document.getElementById("sideNav").style.width = "75%";
+}
+/*Close navigation*/
+function exitNav() {
+  document.getElementById("sideBar").style.width = "0";
+  document.getElementById("sideNav").style.width = "0";
+}
+
+ 
+
+
