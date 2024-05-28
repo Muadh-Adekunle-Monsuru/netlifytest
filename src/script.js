f815fd33a39baa83869dc04089d0909a30242b20
AbdulrahmanLuqman <fal528@dervac.com>
2024-05-28 15:30:27 +0100

    made changes to navbar and added responsiveness to header

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-function openNav() {
-  document.getElementById("sideBar").style.width = "100%";
-  document.getElementById("sideNav").style.width = "75%";
+let hamburger = document.getElementById("hamburger")
+let cancel = document.getElementById("close-icon")
+let cover = document.querySelector(".cover")
+
+hamburger.addEventListener("click", ()=>{
+  document.querySelector(".nav").classList.add("show-nav")
+  cover.classList.add("show-cover")
+})
+
+function closeNav() {
+  document.querySelector(".nav").classList.remove("show-nav")
+  cover.classList.remove("show-cover")
 }
+cancel.addEventListener("click", closeNav)
+cover.addEventListener("click", closeNav)
+
+
 /*Close navigation*/
-function exitNav() {
-  document.getElementById("sideBar").style.width = "0";
-  document.getElementById("sideNav").style.width = "0";
-}
+// function exitNav() {
+//   document.getElementById("sideBar").style.width = "0";
+//   document.getElementById("sideNav").style.width = "0";
+// }
 
  
 
