b564c628dccbf9e94589f67d993357a507b462e1
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-05-29 16:21:53 +0100

    student login

--- src/patients/script.js
+++ src/patients/script.js
@@ -1,3 +1,4 @@
+//Patient Registration and Login
 const handleRegistration = async (event) => {
 	event.preventDefault();
 	const errormessage = document.querySelector('#error-message');
@@ -15,5 +16,25 @@ const handleRegistration = async (event) => {
 		}
 	} catch (e) {
 		console.log('error submitting form' + e);
+	}
+};
+
+const handleLogin = async (event) => {
+	event.preventDefault();
+	const form = document.querySelector('#login-form');
+	const errormessage = document.querySelector('#error-message');
+	try {
+		const response = await fetch('http://localhost:8080/patient/login', {
+			method: 'POST',
+			body: new FormData(form),
+		});
+		const result = await response.json();
+		if (response.ok) {
+			window.location.href = '../patient_dashboard.html';
+		} else {
+			errormessage.innerHTML = result.message;
+		}
+	} catch (e) {
+		console.log('Error logging in ' + e);
 	}
 };
